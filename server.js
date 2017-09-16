6940781c05d93eac99dcb23f2f6ef5e435a0c73b
Matan <20matan@gmail.com>
2017-09-16 14:28:22 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const redis = require("redis")
+
+const app = express()
+const redisClient = redis.createClient();
+const redisKey = 'messages'
+
+redisClient.on('ready', () => {
+  console.log('redis is ready')
+})
+redisClient.on('error', (err) => {
+    console.log('error' + err)
+});
+
+app.use( bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
+// receive message and time
+app.post('/echoAtTime', (req, res) => {
+  const { time, message } = req.body
+  redisClient.zadd([redisKey, time, message], (err, response) => {
+    if (err) {
+      console.log('error was occured', err)
+      res.send('error was occured')
+    } else {
+      res.send('message was added')
+    }
+  })
+})
+app.listen(3000, function () {
+  // every second chek if there's a message to execute
+  setInterval(() => {
+    let currTimestamp = Date.now()
+    let queryParams = [redisKey, 0, currTimestamp]
+
+    // get the messages with timestamp between 0 and current timestamp
+    redisClient.ZRANGEBYSCORE(queryParams, (err, response) => {
+      if (err) {
+        throw err
+      }
+
+      // log the messages
+      response.forEach(message => {
+        console.log(message)
+      })
+
+      // delete the messages
+      redisClient.ZREMRANGEBYSCORE(queryParams, (err, response) => {
+        if (err) {
+          throw err
+        }
+      })
+    })
+  }, 1000)
+})
